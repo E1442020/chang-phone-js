342c7643291779212d9f7a3ee7934ece7b5a1c46
Eman Masoud <114053062+E1442020@users.noreply.github.com>
2023-03-21 11:02:06 +0200

    phone

---
+++ phone.js
@@ -0,0 +1,12 @@
+let img =document.getElementById('img');
+let container =document.querySelector('.container')
+
+function changePhone( src){
+    img.src=src;
+    
+
+}
+
+function changeBackground(color){
+    container.style.background=color
+}
